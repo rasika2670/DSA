2b558d1c2a9bca4d4888820df1d140fb73fd8132
Rasika Mhaske <162533669+rasika2670@users.noreply.github.com>
2026-07-13 20:46:19 +0530

    Time: 0 ms (100%), Space: 9.9 MB (51.24%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) 
+    {
+        priority_queue<int> pq;
+        int n = stones.size();
+
+        for(int st : stones){
+            pq.push(st);
+        }
+
+        while(pq.size()>1)
+        {
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            
+            if(x!=y){
+                pq.push(y-x);
+            }            
+        }
+
+        if (pq.empty()) 
+        {
+            return 0;
+        }
+
+        return pq.top();
+    }
+};
